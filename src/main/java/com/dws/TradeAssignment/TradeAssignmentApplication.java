cf447e5dfa9ab394be3f9cc9130352df87f84291
vivek.c <vchirane@gmail.com>
2024-08-05 23:49:48 +0530

    Scheduling enabled-30 sec, devtools disabled

--- src/main/java/com/dws/TradeAssignment/TradeAssignmentApplication.java
+++ src/main/java/com/dws/TradeAssignment/TradeAssignmentApplication.java
@@ -2,8 +2,10 @@ package com.dws.TradeAssignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TradeAssignmentApplication {
 
 	public static void main(String[] args) {
